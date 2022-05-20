cc617891390727cf70388fa3d6b34040e39493c3
Quinn Elwood <ashes@quinns-mbp-2.lan>
2022-05-19 21:28:27 -0700

    discordjs sucks

--- src/DiscordClient.ts
+++ src/DiscordClient.ts
@@ -1,29 +1,17 @@
-import { REST } from '@discordjs/rest';
-import { Routes } from 'discord-api-types/v9';
+import { Client, Intents } from 'discord.js';
+// import { REST } from '@discordjs/rest';
+// import { Routes } from 'discord-api-types/v9';
 
-const CLIENT_ID = process.env.REACT_APP_DISCORD_CLIENT_ID || '';
-const GUILD_ID = process.env.REACT_APP_DISCORD_GUILD_ID || '';
-
-const commands = [{
-  name: 'ping',
-  description: 'Replies with Pong!'
-}]; 
-
-const rest = new REST({ version: '9' }).setToken('token');
+const TOKEN = process.env.REACT_APP_DISCORD_TOKEN || '';
 
 async function main() {
-  try {
-    console.log('Started refreshing application (/) commands.');
+  const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-    await rest.put(
-      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
-      { body: commands },
-    );
+  // client.once('ready', () => {
+  //   console.log('Ready!');
+  // });
 
-    console.log('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    console.error(error);
-  }
+  client.login(TOKEN);
 };
 
 export default main;
