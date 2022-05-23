10267de66bdb66bddae469b0a9a95c8907418ac6
Quinn Elwood <ashes@quinns-mbp-2.lan>
2022-05-22 20:03:45 -0700

    Getting fancy

--- src/TwitchClient.ts
+++ src/TwitchClient.ts
@@ -3,6 +3,11 @@ import copypasta from './copypasta';
 import { isBefore, differenceInMinutes, differenceInSeconds, addSeconds } from 'date-fns';
 
 const ACTIVE_CHANNEL = 'm60_';
+const COPYPASTA_MINUTE_DELAY = 3;
+const STALE_MESSAGE_DELAY = 45;
+const MESSAGE_MAX_SKIP = 20;
+const MESSAGE_MAX_DELAY = 5;
+
 let ACTIVE_CLIENT: any = null;
 
 const connectToTwitch = async () => {
@@ -28,30 +33,81 @@ async function main() {
 
   listenToMessages(client);
 
-  // say('Hi');
-  // 
-  // sayCopypasta();
   setInterval(() => {
     const seconds = differenceInSeconds(new Date(), clientStartDate) % 60;
     const minutes = differenceInMinutes(new Date(), clientStartDate);
     console.log("Time since start", `${minutes}:${seconds}`)
   }, 1000 * 10)
 
+  
   setInterval(() => {
     sayCopypasta();
-  }, 1000 * 60 * 5);
+  }, 1000 * 60 * COPYPASTA_MINUTE_DELAY);
 
   setInterval(() => {
     sayEnqueuedMessage();
   }, 1000);
 
-  setTimeout(() => {
-    // say('!play')
-    // client.say('m60_', '!play')
-    // client.say('m60_', 'Hi :)');
-  }, 5000)
   return client;
 }
+
+const enqueueResponse = (receivedMessage: string, messageMatch: RegExp, responseMessage: string) => {
+  if (receivedMessage.toLowerCase().match(messageMatch)) {
+    enqueueMessage(responseMessage);
+  }
+};
+
+type TMessageResponse = {
+  regex: RegExp,
+  responses: string[],
+  repeatable?: boolean,
+}
+
+const messageResponses: TMessageResponse[] = [{
+  regex: /^1$/i,
+  responses: ['1'],
+}, {
+  regex: /^LUL$|^LOL$|^HA$/i,
+  responses: ['LUL', 'LOL', 'lmfao', 'Hahaha', 'LULW', 'LMFAO', 'lol', 'lul', 'HAHAHA'],
+}, {
+  regex: /^9\/11$/i,
+  responses: ['11/11'],
+}, {
+  regex: /^Clap/gi,
+  responses: ['Clap'],
+}, {
+  regex: /^poggies/gi,
+  responses: ['POGGIES'],
+}, {
+  regex: /^pogchamp/gi,
+  responses: ['PogChamp'],
+}, {
+  regex: /^PepeHands/gi,
+  responses: ['PepeHands'],
+}, {
+  regex: /duDudu/g,
+  responses: ['duDudu'],
+}, {
+  regex: /^!raid/gi,
+  responses: ['!raid'],
+}, {
+  regex: /^my poops/gi,
+  responses: ['MY POOPS'],
+}, {
+  regex: /^catjam/gi,
+  responses: ['catJAM'],
+  repeatable: true,
+}, {
+  regex: /^peped/gi,
+  responses: ['pepeD'],
+  repeatable: true,
+}, {
+  regex: /^pepehands/gi,
+  responses: ['pepeHands'],
+}, {
+  regex: /^D:|^m60d/gi,
+  responses: ['D:'],
+}];
 
 const listenToMessages = (client: any) => {
   client.on('message', (channel: string, tags: Record<string, string>, message: string, self: boolean) => {
@@ -59,29 +115,14 @@ const listenToMessages = (client: any) => {
 
     const sender = tags['display-name'];
 
-    if(message.toLowerCase().match(/LUL/gi)) {
-      enqueueMessage('LUL');
+    const response = messageResponses.find((mr) => message.match(mr.regex));
+
+    if (response) {
+      const responses = response.responses;
+      enqueueResponse(message, response.regex, responses[Math.floor(Math.random() * responses.length)])
     }
 
-    if(message.toLowerCase().match(/Clap/gi)) {
-      enqueueMessage('Clap');
-    }
-
-    if(message.toLowerCase().match(/poggies/gi)) {
-      enqueueMessage('POGGIES');
-    }
-
-    if(message.toLowerCase().match(/PepeHands/gi)) {
-      enqueueMessage('PepeHands');
-    }
-
-    if(message.toLowerCase().match(/!raid/gi)) {
-      enqueueMessage('!raid');
-    }
-
-    if(message.toLowerCase().match(/channybee/gi)) {
-      say(`Omg hi @${sender}!`);
-    }
+    enqueueResponse(message, /channybee/gi, `Omg hi @${sender}!`);
   });
 }
 
@@ -93,15 +134,38 @@ type TQueuedMessage = {
 const previousMessages: TQueuedMessage[] = [];
 const queuedMessages: TQueuedMessage[] = [];
 
+const getResponseFromMessage = (message: string) => {
+  return messageResponses.find((mr) => mr.responses.includes(message));
+};
+
+const getRecentMessage = (message: string) => {
+  let recentSameMessage: any = previousMessages.find((pm) => pm.message === message);
+  
+  if (!recentSameMessage) {
+    const messageResponse = getResponseFromMessage(message);
+    recentSameMessage = previousMessages.find((pm) => messageResponse?.responses.includes(pm.message));
+  }
+
+  return recentSameMessage;
+};
+
 const isMessageRecent = (message: string) => {
-  const recentSameMessage = previousMessages.find((pm) => pm.message === message);
+  const recentSameMessage = getRecentMessage(message);
 
   if (!recentSameMessage) return false;
   
   const timeDifference = differenceInSeconds(new Date(), recentSameMessage.time);
 
-  return timeDifference < 30;
+  const messageResponse = getResponseFromMessage(message);
+
+  if (messageResponse && messageResponse.repeatable) {
+    return timeDifference < Math.random() * STALE_MESSAGE_DELAY;
+  } 
+
+  return timeDifference < STALE_MESSAGE_DELAY;
 };
+
+let timeSinceLastMessage = new Date();
 
 const enqueueMessage = (message: string) => {
   if (queuedMessages.find((qm) => qm.message === message)) {
@@ -109,12 +173,17 @@ const enqueueMessage = (message: string) => {
   }
 
   if (isMessageRecent(message)) {
-    console.log("EXIT CAUSE OF PREVIOUS MESSAGE")
     return;
   }
 
-  const queueDelay = queuedMessages.length * .7;
-  const randomDelay = (Math.random() * 3) + 1.5 + queueDelay;
+
+  if(differenceInSeconds(new Date(), timeSinceLastMessage) < Math.random() * MESSAGE_MAX_SKIP) {
+    return;
+  } 
+
+  timeSinceLastMessage = new Date();
+
+  const randomDelay = (Math.random() * MESSAGE_MAX_DELAY) + 1;
   queuedMessages.push({
     message,
     time: addSeconds(new Date(), randomDelay),
